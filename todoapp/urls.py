7fdc6a6a1d8791e88d94300a08bd42ed33145b83
dvnagesh <dvnagesh@gmail.com>
2017-10-22 21:37:39 +0530

    Modified templates to display todos from database.

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.todo_list, name='todo_list'),
 	url(r'^todo/(?P<pk>\d+)/$', views.todo_detail, name='todo_detail'),
+	url(r'^todo/new/$', views.todo_new, name='todo_new'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 from django.utils import timezone
 from .models import Todo
+from .forms import TodoForm
 
 def todo_list(request):
 	todos = Todo.objects.filter(published_date__lte=timezone.now()).order_by('due_date')
@@ -11,3 +12,7 @@ def todo_list(request):
 def todo_detail(request, pk):
 	todo = get_object_or_404(Todo, pk=pk)
 	return render(request, 'todoapp/todo_detail.html', {'todo': todo})
+
+def todo_new(request):
+	form = TodoForm()
+	return render(request, 'todoapp/todo_edit.html', {'form': form})
